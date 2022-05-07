af96e27f9b5eed3d6e45e458acc4717ac237bc35
Mirtos <ifds9999@gmail.com>
2022-05-07 04:50:15 -0300

    07/05

---
+++ resources/views/includes/subviews/footer.blade.php
@@ -0,0 +1,23 @@
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Give me a Job Please 2022</p>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="{{asset('js/jquery.js')}}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+
+</body>
+
+</html>
